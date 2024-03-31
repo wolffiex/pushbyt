f053fac039fde908a95a522d377d7567aa927c2b
Adam Wolff <awolff@gmail.com>
2024-03-31 14:52:15 -0700

    animating

--- src/main.py
+++ src/main.py
@@ -1,14 +1,37 @@
 import argparse
+from webptools import webpmux_animate
 import base64
-import json
 import os
 import requests
 from io import BytesIO
 from PIL import Image
-import logging
+from pathlib import Path
+import tempfile
+
 
 TIDBYT_API_PUSH = "https://api.tidbyt.com/v0/devices/%s/push"
+sprites = Path("./sprites")
+frame_files = [f for f in sorted(os.listdir(sprites)) if f.startswith("frame")]
 
+tempfiles = []
+for file in frame_files:
+    png_image = Image.open(sprites / file)
+    with tempfile.NamedTemporaryFile(suffix=".webp", delete=False) as temp_file:
+        # Save the image as WebP format
+        png_image.save(temp_file.name, "WebP")
+        tempfiles.append(temp_file.name +" +100")
+
+# print(webpmux_animate(input_images=tempfiles, output_image="anim_container.webp",
+#                       loop="10", bgcolor="255,255,255,255"))
+
+frames = " ".join(f'-frame {f}' for f in tempfiles)
+cmd = f"webpmux {frames} -loop 10 -bgcolor 255,255,255,255 -o out.webp"
+print(cmd)
+
+# webpmux  -frame /var/folders/3v/jl2nyhvx0fv2qfy6yz4fh9cw0000gn/T/tmp_bp7j2zq.webp +100 
+#           -frame /var/folders/3v/jl2nyhvx0fv2qfy6yz4fh9cw0000gn/T/tmp2c4idylv.webp +100
+#           -frame /var/folders/3v/jl2nyhvx0fv2qfy6yz4fh9cw0000gn/T/tmpx821h6zz.webp +100 
+#           -loop 10 -bgcolor 255,255,255,255 -o anim_container.webp
 def push(device_id, installation_id, background):
     api_token = os.getenv("TIDBYT_TOKEN")
 
@@ -34,6 +57,8 @@ def push(device_id, installation_id, background):
         raise ValueError(f"Tidbyt API returned status: {response.status_code}")
 
 def create():
+    with open("out.webp", "rb") as anim:
+        return anim.read()
     # Create a new image with a black background
     width, height = 64, 32
     image = Image.new("RGB", (width, height), "black")
@@ -44,7 +69,23 @@ def create():
     # Draw a 10x10 white box offset by 10 pixels on each axis
     for i in range(10, 20):
         for j in range(10, 20):
-            pixels[i, j] = (255, 0, 255)  # Set pixel to white
+            pixels[i, j] = (255, 0, 0)  # Set pixel to white
+
+    png_image = Image.open("sprites/Sprite.png")
+
+    # Create a new list to store the frames
+    frames = [image, png_image]
+
+    # Save the frames as an animated WebP to a BytesIO object
+    image_buffer = BytesIO()
+    frames[0].save(
+        image_buffer,
+        "WebP",
+        save_all=True,
+        append_images=frames[1:],
+        duration=1000,  # Duration of each frame in milliseconds
+        loop=0,  # Set the loop count (0 for infinite loop)
+    )
 
     # Save the image to a BytesIO object
     image_buffer = BytesIO()
@@ -59,4 +100,37 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     push(args.device_id, args.installation_id, args.background)
+
+
+# pass input_images(.webp image) path with FRAME_OPTIONS, as array,ouput image will be animated .webp image
+
+# https://developers.google.com/speed/webp/docs/webpmux
+# FRAME_OPTIONS
+
+# -file_i +di[+xi+yi[+mi[bi]]]
+
+# e.g -frame one.webp +100 -frame two.webp +100+50+50 -frame three.webp +100+50+50+1+b
+
+# Where: file_i is the i'th frame (WebP format), xi,yi specify the image offset for this frame,
+# di is the pause duration before next frame, mi is the dispose method for this frame (0 for NONE or 1 for BACKGROUND)
+# and bi is the blending method for this frame (+b for BLEND or -b for NO_BLEND).
+# Argument bi can be omitted and will default to +b (BLEND). Also, mi can be omitted if bi is omitted and
+# will default to 0 (NONE). Finally,
+# if mi and bi are omitted then xi and yi can be omitted and will default to +0+0.
+
+# -loop n
+
+# e.g 10
+
+# Loop the frames n number of times. 0 indicates the frames should loop forever.
+# Valid range is 0 to 65535 [Default: 0 (infinite)].
+
+# -bgcolor A,R,G,B
+
+# e.g 255,255,255,255
+
+# Background color of the canvas. Where: A, R, G and B are integers in the range 0 to 255 specifying
+# the Alpha, Red, Green and Blue component values respectively [Default: 255,255,255,255].
+
+# now read anim_container file and return as BtyesIO
 
