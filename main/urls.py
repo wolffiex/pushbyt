3edb1ea65d044a218e0308b548dfd038fc492873
Adam Wolff <adam@reginald>
2024-04-13 16:47:46 -0700

    Start of player

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from main.views import get_webp
+from main.views import get_webp, get_player
 
 urlpatterns = [
     path("anim/<str:name>", get_webp, name="get_webp"),
+    path("player/<str:name>", get_player, name="get_player"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,9 @@
 from django.http import Http404, HttpResponse
+from django.shortcuts import render
 from main.animation.rays import clock_rays
 from io import BytesIO
 import logging
+import base64
 
 logger = logging.getLogger(__name__)
 
@@ -12,7 +14,6 @@ def get_webp(request, name:str):
         frames = clock_rays()
 
     if frames:
-
         output = BytesIO()
         # Save the image to the response
         frames[0].save(
@@ -30,3 +31,21 @@ def get_webp(request, name:str):
         return response
 
     raise Http404(f"Unknown animation {name}")
+
+def get_player(request, name:str):
+    frames = None
+    if name == "rays":
+        frames = clock_rays()
+        frame_bytes = (encode_frame(frame) for frame in frames)
+        return render(request, "player.html", {"frames": frame_bytes, "title": name})
+
+    raise Http404(f"Unknown animation {name}")
+
+def encode_frame(frame):
+    output = BytesIO()
+    frame.save(
+        output,
+        format="WEBP",
+        quality=100,
+    )
+    return base64.b64encode(output.getvalue()).decode('utf-8')
